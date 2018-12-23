d54516434649c3b66b6bedf1c40c2d110061d0d1
Nico <nico.niebergall@googlemail.com>
2018-12-23 11:55:42 +0100

    Added: Configuration for creating the task definition and service

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,5 +13,28 @@ resource "aws_ecr_repository" "repository" {
 }
 
 resource "aws_ecs_cluster" "cluster" {
-  name = "automation-playground-development"
+  name = "automation-playground"
 }
+
+# Task definition
+data "template_file" "task-definition-template" {
+  template = "${file("task-definitions/staging.tpl")}"
+
+  vars {
+    image = "${aws_ecr_repository.repository.repository_url}/${var.project-name}:latest"
+  }
+}
+
+resource "aws_ecs_task_definition" "task-definition" {
+  container_definitions = "${data.template_file.task-definition-template.rendered}"
+  family = "staging"
+}
+
+resource "aws_ecs_service" "service" {
+  name = "staging"
+  task_definition = "staging"
+  launch_type = "fargate"
+}
+
+
+
