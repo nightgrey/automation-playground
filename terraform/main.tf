8221a073bbf343f395cd605b487d0c3afed9a712
Nico <nico.niebergall@googlemail.com>
2018-12-24 17:33:45 +0100

    Added: Building and pushing the docker image to ECR, finished ECS configuration in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,84 @@
-variable "project-name" {
-  type = "string"
-  default = "automation-playground"
-}
-
 provider "aws" {
   region = "eu-central-1"
-  shared_credentials_file = "$HOME/.aws/credentials"
 }
 
+# Networking
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
+
+  tags {
+    project = "${var.project-name}"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    project = "${var.project-name}"
+  }
+}
+
+resource "aws_internet_gateway" "internet-gatway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    project = "${var.project-name}"
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    project = "${var.project-name}"
+  }
+}
+
+resource "aws_route" "internet-gateway" {
+  route_table_id         = "${aws_route_table.route-table.id}"
+  gateway_id             = "${aws_internet_gateway.internet-gatway.id}"
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Security groups
+resource "aws_security_group" "security-group" {
+  name        = "${var.project-name}"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags {
+    project = "${var.project-name}"
+  }
+}
+
+
+# ECS
 resource "aws_ecr_repository" "repository" {
   name = "${var.project-name}"
 }
 
 resource "aws_ecs_cluster" "cluster" {
-  name = "automation-playground"
+  name = "${var.project-name}"
 }
 
-# Task definition
 data "template_file" "task-definition-template" {
-  template = "${file("task-definitions/staging.tpl")}"
+  template = "${file("task-definitions/staging.json")}"
 
   vars {
     image = "${aws_ecr_repository.repository.repository_url}/${var.project-name}:latest"
@@ -28,13 +88,29 @@ data "template_file" "task-definition-template" {
 resource "aws_ecs_task_definition" "task-definition" {
   container_definitions = "${data.template_file.task-definition-template.rendered}"
   family = "staging"
+  network_mode = "awsvpc"
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = "ecsTaskExecutionRole"
+  task_role_arn = "ecsTaskExecutionRole"
+  requires_compatibilities = [
+    "FARGATE"
+  ]
 }
 
 resource "aws_ecs_service" "service" {
-  name = "staging"
-  task_definition = "staging"
-  launch_type = "fargate"
+  name            = "staging"
+  cluster         = "${aws_ecs_cluster.cluster.id}"
+  task_definition = "${aws_ecs_task_definition.task-definition.arn}"
+  desired_count   = 1
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      "${aws_subnet.subnet.id}"
+    ]
+    assign_public_ip = true
+    security_groups = []
+  }
 }
-
-
 
